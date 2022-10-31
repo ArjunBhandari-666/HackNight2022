893d6c73197c36a6c2d86f381f61331edc3fbd3c
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2022-10-31 09:46:31 +0530

    Update Dashboard.js

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import './Dashboard.css'
 const Dashboard = () => {
+  useEffect(() => {
+    fetch("https://api.airtable.com/v0/appEbfK98XiSAoltm/records?api_key=keycoBS9EC4zmk6Wr")
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data.records);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const [recipes, setRecipes] = useState({});
   return (
     <div className='dashboard'>
     <h1>HISTORY</h1>
     <div className='dashboard-container'>
        
-            <div className='card'>
+            {/* <div className='card'>
                 <p><span>Name :</span>Mr. Ramesh </p>
                 <p><span>Checkup Date:</span> 31-10-2022</p>
                 <p><span>Checkup at :</span> AJ Hospital</p>
@@ -15,23 +27,17 @@ const Dashboard = () => {
                 <p><span>Medicine prescribed :</span> Cilacar (5mg)</p>
 
             </div>
-            <div className='card'>
-                <p><span>Name :</span>Mr. Ramesh </p>
-                <p><span>Checkup Date:</span> 15-11-2022</p>
-                <p><span>Checkup at :</span> AJ Hospital</p>
-                <p><span>Doctors Name :</span> Dr. Kishan </p>
-                <p><span>Summary :</span> Blood pressure not normal but better compared to last checkup</p>
-                <p><span>Medicine prescribed :</span> Cilacar (5mg)</p>
-            </div>
-            <div className='card'>
-                <p><span>Name :</span>Mr. Ramesh </p>
-                <p><span>Checkup Date:</span> 30-11-2022</p>
-                <p><span>Checkup at :</span> AJ Hospital</p>
-                <p><span>Doctors Name :</span> Dr. Ram </p>
-                <p><span>Summary :</span> Blood pressure back to normal levels</p>
-                <p><span>Medicine prescribed :</span>None</p>
-            </div>
-           
+        */}
+          {recipes.length > 0 ? (
+      recipes.map((record) => (
+        <a href={record.fields.url} key={record.id}>
+          {/* <img src={record.fields.photo[0].url} alt={record.fields.recipe} /> */}
+          <p>{record.fields.Name}</p>
+        </a>
+      ))
+    ) : (
+      <p>Fetching Data...</p>
+    )}
     </div>
     </div>
   )
